3ac0e479c0c9a9078db8bd2543930d20ab60a392
Kings Uthana Idogu <kingsuthanaidogu@gmail.com>
2025-02-02 07:08:11 +0000

    Chatbots

--- files/ChatBotApp.py
+++ files/ChatBotApp.py
@@ -1,3 +1,40 @@
 import gradio as gr
 
-gr.load_chat
+gr.load_chat("http://localhost:11434/v1/", model="llama3.2", token="ollama").launch()
+
+
+# import random
+
+# def random_response(message, history):
+#     return random.choice(["Yes", "No"])
+
+# gr.ChatInterface(
+#     fn=random_response,
+#     type="messages"
+#     ).lunch()
+
+def alternatingly_agree(message, history):
+    if len([h for h in history if h['role'] == "assistant"]) % 2 == 0:
+        return f"Yes, I do think that: {message}"
+    else:
+        return "I don't think so"
+    
+gr.ChatInterface(
+    fn=alternatingly_agree,
+    type="messages"
+).launch()
+
+
+# Streaming ChatBot
+import time 
+import gradio as gr
+
+def slow_echo(message, history):
+    for i in range(len(message)):
+        time.sleep(0.3)
+        yield "You typed: " + message[: i+1]
+
+gr.ChatInterface(
+    fn=slow_echo,
+    type="messages"
+).launch()
